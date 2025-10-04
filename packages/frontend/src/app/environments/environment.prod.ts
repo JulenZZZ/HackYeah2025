c0c483642096b0a308902106c51663bc92d2e3a3
Arek <arkadiusz.pabian@gmail.com>
2025-10-04 21:37:13 +0200

    chore: add environment variables and generic api call

---
+++ packages/frontend/src/app/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://147.93.72.233:8000/api/',
+  username: 'root',
+  password: 'root',
+}
---
+++ packages/frontend/src/app/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  //apiUrl: 'http://localhost:8000/api/'
+  apiUrl: 'http://147.93.72.233:8000/api/',
+  username: 'root',
+  password: 'root',
+}
