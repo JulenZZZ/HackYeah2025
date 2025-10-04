7a2769bd663b7084cf32ec4036b078f1a50d91a2
Arek <arkadiusz.pabian@gmail.com>
2025-10-04 23:05:33 +0200

    feat: add challenge categories

--- packages/frontend/src/app/services/game-state.ts
+++ packages/frontend/src/app/services/game-state.ts
@@ -1,17 +1,50 @@
 import { Injectable } from '@angular/core'
+import { ChallengeCategory } from '../models/challenge-category.enum'
+import { GameMode } from '../models/game-mode.enum'
 import { Sex } from '../models/sex.enum'
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameStateService {
+  // Prywatne zmienne z konwencyjnym prefixem '_'
   private _charSex: Sex | undefined
+  private _gameMode: GameMode | undefined
+  private _challengeCategory: ChallengeCategory | undefined
 
-  get charSex() {
+  /**
+   * Getter dla płci postaci
+   */
+  get charSex(): Sex | undefined {
     return this._charSex
   }
 
+  /**
+   * Setter (metoda) dla płci postaci
+   */
   setCharSex(sex: Sex) {
     this._charSex = sex
+  }
+
+  /**
+   * Getter dla aktualnego trybu gry
+   */
+  get gameMode() {
+    return this._gameMode
+  }
+
+  setGameMode(gameMode: GameMode) {
+    this._gameMode = gameMode
+  }
+
+  /**
+   * Getter dla kategorii wyzwania
+   */
+  get challengeCategory() {
+    return this._challengeCategory
+  }
+
+  setChallengeCategory(challengeCategory: ChallengeCategory) {
+    this._challengeCategory = challengeCategory
   }
 }
