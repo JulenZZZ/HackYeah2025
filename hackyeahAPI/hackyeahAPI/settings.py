33b66bb9b11127187e4b4a9e77d865a23423ec60
szymonglowka <szymonglwk@gmail.com>
2025-10-04 22:04:42 +0200

    deleted auth

--- hackyeahAPI/hackyeahAPI/settings.py
+++ hackyeahAPI/hackyeahAPI/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
     'game_rules',
     'character',
     'game_sessions'
@@ -135,9 +134,3 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-}
--- hackyeahAPI/hackyeahAPI/urls.py
+++ hackyeahAPI/hackyeahAPI/urls.py
@@ -3,11 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 # Importujemy routery z każdej aplikacji
 from game_sessions.urls import router as sessions_router
 from game_rules.urls import router as rules_router
@@ -24,8 +19,4 @@ router.registry.extend(character_router.registry)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-
-    # Endpointy do uwierzytelniania JWT
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
