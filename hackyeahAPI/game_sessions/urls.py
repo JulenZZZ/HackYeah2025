b2699f69336547fde9ed2302c97636117a5b0c61
szymonglowka <szymonglwk@gmail.com>
2025-10-04 18:27:44 +0200

    unified api paths to one path

--- hackyeahAPI/game_sessions/urls.py
+++ hackyeahAPI/game_sessions/urls.py
@@ -1,11 +1,6 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import GameSessionViewSet, CharacterStateViewSet
 
 router = DefaultRouter()
 router.register(r'sessions', GameSessionViewSet, basename='gamesession')
 router.register(r'character-states', CharacterStateViewSet, basename='characterstate')
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
