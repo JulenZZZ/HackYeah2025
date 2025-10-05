c3e4a646110531874d4954fb4105b8cf122d0a03
szymonglowka <szymonglwk@gmail.com>
2025-10-05 02:17:25 +0200

    front django

--- hackyeahAPI/game_rules/views.py
+++ hackyeahAPI/game_rules/views.py
@@ -1,3 +1,6 @@
+# hackyeahAPI/game_rules/views.py
+
+from django.shortcuts import render
 from rest_framework import viewsets
 from .models import LifeStage
 from .serializers import LifeStageSerializer
@@ -20,3 +23,14 @@ class GameRulesViewSet(viewsets.ReadOnlyModelViewSet):
       'questions__answers__conditions__attribute',
       'questions__answers__impacts__attribute'
     ).all()
+
+
+# NOWY WIDOK
+def game_rules_view(request):
+    """
+    Widok renderujący szablon HTML z całą strukturą gry.
+    """
+    life_stages = LifeStage.objects.prefetch_related(
+        'questions__answers'
+    ).all()
+    return render(request, 'game_rules/index.html', {'life_stages': life_stages})
