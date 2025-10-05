5581452258c1288a2e0e28a8c6b6f0f1cd9143fb
szymonglowka <szymonglwk@gmail.com>
2025-10-05 03:49:23 +0200

    new views

--- hackyeahAPI/game/urls.py
+++ hackyeahAPI/game/urls.py
@@ -1,6 +1,25 @@
+# hackyeahAPI/game/urls.py
+
 from django.urls import path
-from .views import game_view
+from .views import (
+    game_view,
+    LandingPageView,
+    CharacterSelectView,
+    ChallengeSelectView,
+)
 
 urlpatterns = [
-    path('', game_view, name='game-view'),
+    # Ścieżka do istniejącego widoku gry
+    path('play/', game_view, name='play_game'),
+
+    # --- NOWE ŚCIEŻKI ---
+
+    # 1. Landing Page
+    path('', LandingPageView.as_view(), name='landing_page'),
+
+    # 2. Wybór Postaci
+    path('character-select/', CharacterSelectView.as_view(), name='character_select'),
+
+    # 3. Wybór Wyzwania
+    path('challenge-select/', ChallengeSelectView.as_view(), name='challenge_select'),
 ]
